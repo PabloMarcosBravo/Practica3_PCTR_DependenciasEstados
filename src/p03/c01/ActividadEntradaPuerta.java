9f3a5b91859b86a075042ca7556718df3f84987c
Pablo Marcos Bravo <pmb1003@alu.ubu.es>
2022-03-29 09:45:10 +0200

    Comentarios de la clase de entrada realizados

--- src/p03/c01/ActividadEntradaPuerta.java
+++ src/p03/c01/ActividadEntradaPuerta.java
@@ -12,29 +12,38 @@ import java.util.logging.Logger;
  * @version 2.0
  */
 
-public class ActividadEntradaPuerta implements Runnable{
+public class ActividadEntradaPuerta implements Runnable {
 
-		private static final int NUMENTRADAS = 20;
-		private String puerta;
-		private IParque parque;
+	private static final int NUMENTRADAS = 20;
+	private String puerta;
+	private IParque parque;
 
-		public ActividadEntradaPuerta(String puerta, IParque parque) {
-			this.puerta = puerta;
-			this.parque = parque;
-		}
+	/**
+	 * Constructor de la clase
+	 * 
+	 * @param puerta por la que se quiere acceder al parque
+	 * @param parque donde se realiza la acttividad de entrada
+	 */
+	public ActividadEntradaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
 
-		@Override
-		public void run() {
-			for (int i = 0; i < NUMENTRADAS; i ++) {
-				try {
-					parque.entrarAlParque(puerta);
-					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
-				} catch (InterruptedException e) {
-					Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
-					Logger.getGlobal().log(Level.INFO, e.toString());
-					return;
-				}
+	@Override
+	/**
+	 * Metodo que ejecuta la entrada al parque por una puerta
+	 */
+	public void run() {
+		for (int i = 0; i < NUMENTRADAS; i++) {
+			try {
+				parque.entrarAlParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
 			}
 		}
+	}
 
 }
