48dc6f88a5eb06ffdb7a62c4ae90bae33c321817
jorge <jorge@DESKTOP-431LRG7>
2022-03-29 09:26:35 +0200

    Modificacion en la clase ActividadSalidaParque

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -7,25 +7,36 @@ import java.util.logging.Logger;
 
 /**
  * @author Pablo Marcos Bravo
+ * @author Jorge Fernandez Moreno
  * @since JDK 11
  * @version 2.0
  */
 
 public class ActividadSalidaPuerta implements Runnable{
+	
 	private static final int NUMSALIDAS = 20;
 	private String puerta;
 	private IParque parque;
 	
+	/**
+	* Constructor
+	* 
+	* @param puerta de salida del parque
+	* @param parque sobre el que nos encontramos
+	*/
 	public ActividadSalidaPuerta(String puerta, IParque parque) {
 		this.puerta= puerta;
 		this.parque = parque;
 	}
 
+	/**
+	 * Metodo que saca a personas del parque
+	 */
 	@Override
 	public void run() {
 		for (int i = 0; i < NUMSALIDAS; i ++) {
 			try {
-				parque.entrarAlParque(puerta);
+				parque.salirDelParque(puerta);
 				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
 			} catch (InterruptedException e) {
 				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
